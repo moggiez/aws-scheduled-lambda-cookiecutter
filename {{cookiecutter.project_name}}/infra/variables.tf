a87880b478a8684ab5a32548bfd3464017109cb4
GeorgiStavrev <georgi@insurify.com>
2021-08-21 10:22:13 +0300

    adds lambda permissions and fixes terraform formatting

--- {{cookiecutter.project_name}}/infra/variables.tf
+++ {{cookiecutter.project_name}}/infra/variables.tf
@@ -1,19 +1,19 @@
 variable "domain_name" {
-    type    = string
-    default = "{{cookiecutter.domain_name}}"
+  type    = string
+  default = "{{cookiecutter.domain_name}}"
 }
 
 variable "project_name" {
-    type    = string
-    default = "{{cookiecutter.project_name|replace(" ", "_")|replace("-", "_")}}"
+  type    = string
+  default = "{{cookiecutter.project_name|replace(" ", "_")|replace("-", "_")}}"
 }
 
 variable "account" {
-    type    = string
-    default = "{{cookiecutter.aws_account_id}}"
+  type    = string
+  default = "{{cookiecutter.aws_account_id}}"
 }
 
 variable "region" {
-    type    = string
-    default = "{{cookiecutter.aws_region}}"
+  type    = string
+  default = "{{cookiecutter.aws_region}}"
 }
